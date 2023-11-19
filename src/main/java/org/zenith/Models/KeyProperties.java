0d1881ee20529af3aaa74a853091d85903001be8
Dennis Moes <dennismoes@me.com>
2023-11-19 19:35:02 +0100

    createKey also saves the IV and reads the IV and secret separate

---
+++ src/main/java/org/zenith/Models/KeyProperties.java
@@ -0,0 +1,24 @@
+package org.zenith.Models;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class KeyProperties {
+    private SecretKey secretKey;
+    private byte[] nonce;
+
+    public KeyProperties(byte[] secret, byte[] nonce) {
+        this.secretKey = new SecretKeySpec(secret, "AES");
+        this.nonce = nonce;
+    }
+
+    public SecretKey getSecretKey() {
+        return secretKey;
+    }
+
+    public byte[] getNonce() {
+        return nonce;
+    }
+}
--- src/main/java/org/zenith/Utilities/KeyUtils.java
+++ src/main/java/org/zenith/Utilities/KeyUtils.java
@@ -1,5 +1,6 @@
 package org.zenith.Utilities;
 
+import org.zenith.Models.KeyProperties;
 import org.zenith.Properties;
 
 import javax.crypto.KeyGenerator;
@@ -16,11 +17,19 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class KeyUtils {
+    // TODO: Add constructor with interface to pass path as a parameter. This will also help with testing
+
     private String getPath() {
         return Properties.getProperty("keyFolderPath");
     }
 
-    public void createKey(String keyName) {
+    private byte[] getRandomNonce() {
+        byte[] nonce = new byte[12];
+        new SecureRandom().nextBytes(nonce);
+        return nonce;
+    }
+
+    public Path createKey(String keyName) {
         try {
             Path dirPath = Paths.get(getPath());
 
@@ -41,16 +50,25 @@ public class KeyUtils {
             // Size 128, 192 or 256
             keyGen.init(256, SecureRandom.getInstanceStrong());
 
-            String secret = new String(keyGen.generateKey().getEncoded(), StandardCharsets.UTF_8);
+            byte[] secret = keyGen.generateKey().getEncoded();
+            byte[] nonce = getRandomNonce();
 
-            // TODO: Run test to see if you need to write as bytes not as string
-            Files.writeString(keyPath, secret);
+            byte[] content = new byte[nonce.length + secret.length];
+
+            for (int i = 0; i < content.length; i++) {
+                content[i] = i < nonce.length ? nonce[i] : secret[i - nonce.length];
+            }
+
+            // TODO: Change this to two separate files. One with .key and one with .iv
+            //  This to make it easier to retrieve the data
+            return Files.write(keyPath, content);
         } catch (IOException | NoSuchAlgorithmException ex) {
             ex.printStackTrace();
+            return null;
         }
     }
 
-    public SecretKey getKey(String keyName) {
+    public KeyProperties getKey(String keyName) {
         try {
             Path keyPath = Paths.get(String.format("%s/%s.key", getPath(), keyName));
 
@@ -58,20 +76,37 @@ public class KeyUtils {
                 throw new FileNotFoundException(keyPath.toString());
             }
 
-            return new SecretKeySpec(Files.readAllBytes(keyPath), "AES");
+            byte[] keyContent = Files.readAllBytes(keyPath);
+
+            byte[] nonce = new byte[12];
+            byte[] secret = new byte[keyContent.length - 12];
+
+            for (int i = 0; i < keyContent.length; i++) {
+                if (i < 12) {
+                    nonce[i] = keyContent[i];
+                } else {
+                    secret[i - nonce.length] = keyContent[i];
+                }
+            }
+
+//            System.out.println(Arrays.toString(nonce));
+//            System.out.println(Arrays.toString(secret));
+
+            return new KeyProperties(secret, nonce);
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
     }
 
-    public void deleteKey(String keyName) {
+    public boolean deleteKey(String keyName) {
         try {
             Path keyPath = Paths.get(String.format("%s/%s.key", getPath(), keyName));
 
-            Files.deleteIfExists(keyPath);
+            return Files.deleteIfExists(keyPath);
         } catch (IOException ex) {
             ex.printStackTrace();
+            return false;
         }
     }
 
