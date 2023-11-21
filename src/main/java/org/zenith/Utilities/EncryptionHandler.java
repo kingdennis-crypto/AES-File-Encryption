ffac27674a6995aae97ffb59e1ec6a57dd38738a
Dennis Moes <dennismoes@me.com>
2023-11-21 17:42:30 +0100

    Improve: Added some exception handling

--- src/main/java/org/zenith/Utilities/EncryptionHandler.java
+++ src/main/java/org/zenith/Utilities/EncryptionHandler.java
@@ -1,18 +1,26 @@
 package org.zenith.Utilities;
 
+import org.zenith.Models.KeyProperties;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
 
 public class EncryptionHandler {
-    public byte[] encrypt(byte[] plain, SecretKey secret, byte[] iv) {
+    public byte[] encrypt(byte[] plain, SecretKey secret, byte[] iv) throws InvalidParameterException {
         try {
+            if (plain == null || secret == null || iv == null)
+                throw new InvalidParameterException();
+
             Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
             cipher.init(Cipher.ENCRYPT_MODE, secret, new GCMParameterSpec(128, iv));
 
             return cipher.doFinal(plain);
+        } catch(InvalidParameterException ex) {
+            throw ex;
         } catch (Exception ex) {
             ex.printStackTrace();
             return null;
