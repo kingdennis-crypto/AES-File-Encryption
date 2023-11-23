aac9c5a8bb5c3bc49f796e2eac50c86e4e3a1716
Dennis Moes <dennismoes@me.com>
2023-11-23 10:06:30 +0100

    Docs: Added JavaDoc to the EncryptionHandler

--- src/main/java/org/zenith/Handlers/EncryptionHandler.java
+++ src/main/java/org/zenith/Handlers/EncryptionHandler.java
@@ -1,21 +1,24 @@
 package org.zenith.Handlers;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.InvalidParameterException;
 import java.util.Base64;
 
 public class EncryptionHandler {
     /**
      * Encrypts the given byte array using the AES/GCM/NoPadding algorithm.
-     * @param plain The byte array to be encrypted.
+     *
+     * @param plain  The byte array to be encrypted.
      * @param secret The secret key used for the encryption.
-     * @param iv The initialization vector for encryption.
+     * @param iv     The initialization vector for encryption.
      * @return The encrypted byte array.
      * @throws InvalidParameterException If any of the input parameters is null.
      */
@@ -29,7 +32,7 @@ public class EncryptionHandler {
             byte[] cipherText = cipher.doFinal(plain);
 
             return Base64.getEncoder().encodeToString(cipherText);
-        } catch(InvalidParameterException ex) {
+        } catch (InvalidParameterException ex) {
             throw ex;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -37,13 +40,17 @@ public class EncryptionHandler {
         }
     }
 
-    public void encryptFile(String path, SecretKey secret, byte[] iv) {
+    /**
+     * Reads data from a file, processes using the specified {@link Cipher}, and writes the result back to the file.
+     *
+     * @param cipher      The cipher used for encryption and decryption.
+     * @param path        The path to the input file.
+     * @param willEncrypt A boolean indicating whether the data will be encrypted or decrypted.
+     */
+    private void readAndWriteToFile(Cipher cipher, String path, boolean willEncrypt) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-            cipher.init(Cipher.ENCRYPT_MODE, secret, new GCMParameterSpec(128, iv));
-
-            File inputFile = new File(path);
-            File outputFile = new File(path + ".encrypted");
+            File inputFile = new File(willEncrypt ? path : path + ".encrypted");
+            File outputFile = new File(willEncrypt ? path + ".encrypted" : path);
 
             FileInputStream inputStream = new FileInputStream(inputFile);
             FileOutputStream outputStream = new FileOutputStream(outputFile);
@@ -53,6 +60,7 @@ public class EncryptionHandler {
 
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 byte[] output = cipher.update(buffer, 0, bytesRead);
+
                 if (output != null) {
                     outputStream.write(output);
                 }
@@ -65,57 +73,53 @@ public class EncryptionHandler {
 
             inputStream.close();
             outputStream.close();
+        } catch (IllegalBlockSizeException | BadPaddingException | IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Encrypts a file using AES/GCM/NoPadding algorithm.
+     *
+     * @param path   The path to the file to be encrypted.
+     * @param secret The secret key used for encryption.
+     * @param iv     The initialization vector for encryption
+     */
+    public void encryptFile(String path, SecretKey secret, byte[] iv) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, secret, new GCMParameterSpec(128, iv));
+
+            readAndWriteToFile(cipher, path, true);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public void decryptFile(String path, SecretKey secret, byte[] iv) { {
+    /**
+     * Decrypts a file using the AES/GCM/NoPadding algorithm.
+     *
+     * @param path   The path to the file to be encrypted.
+     * @param secret The secret key used for decryption.
+     * @param iv     The initialization vector for decryption.
+     */
+    public void decryptFile(String path, SecretKey secret, byte[] iv) {
         try {
             Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
             cipher.init(Cipher.DECRYPT_MODE, secret, new GCMParameterSpec(128, iv));
 
-            File inputFile = new File(path + ".encrypted");
-            File outputFile = new File(path);
+            readAndWriteToFile(cipher, path, false);
 
-            FileInputStream inputStream = new FileInputStream(inputFile);
-            FileOutputStream outputStream = new FileOutputStream(outputFile);
-
-            byte[] buffer = new byte[64];
-            int bytesRead;
-
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                byte[] output = cipher.update(buffer, 0, bytesRead);
-                if (output != null) {
-                    outputStream.write(output);
-                }
-            }
-
-            byte[] hello = "Hello again!".getBytes();
-            byte[] outputBytes = cipher.doFinal();
-
-            byte[] output = new byte[hello.length + outputBytes.length];
-
-            for (int i = 0; i < output.length; i++) {
-                output[i] = i < outputBytes.length ? outputBytes[i] : hello[i - outputBytes.length];
-            }
-
-
-            if (outputBytes != null) {
-                outputStream.write(output);
-            }
-
-            inputStream.close();
-            outputStream.close();
+            // Removes the encrypted file after decryption
+            Files.delete(Path.of(path + ".encrypted"));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    }
-
     /**
      * Converts a byte array to its hexadecimal representation.
+     *
      * @param bytes The byte array to be represented in hexadecimal.
      * @return The hexadecimal representation
      */
@@ -129,9 +133,10 @@ public class EncryptionHandler {
 
     /**
      * Decrypts the given byte array using the AES/GCM/NoPadding algorithm.
+     *
      * @param encrypt The encrypted byte array to be decrypted.
-     * @param secret The secret key used for the encryption.
-     * @param iv The initialization vector for encryption.
+     * @param secret  The secret key used for the encryption.
+     * @param iv      The initialization vector for encryption.
      * @return The decrypted string.
      * @throws InvalidParameterException If any of the input parameters is null.
      */
@@ -145,10 +150,10 @@ public class EncryptionHandler {
 
 //            byte[] decryptedBytes = cipher.doFinal(encrypt);
             byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encrypt));
-            String decrypted =  new String(decryptedBytes, StandardCharsets.UTF_8);
+            String decrypted = new String(decryptedBytes, StandardCharsets.UTF_8);
 
             return decrypted;
-        } catch(InvalidParameterException ex) {
+        } catch (InvalidParameterException ex) {
             throw ex;
         } catch (Exception ex) {
             ex.printStackTrace();
