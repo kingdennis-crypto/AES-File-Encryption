6e72588039f109250ceda25757f933bca03b7dc4
Dennis Moes <dennismoes@me.com>
2023-11-24 10:10:48 +0100

    Improve: Changed the class to a singleton and added a function to ask the user key folder

--- src/main/java/org/zenith/Handlers/ConfigurationHandler.java
+++ src/main/java/org/zenith/Handlers/ConfigurationHandler.java
@@ -1,27 +1,68 @@
 package org.zenith.Handlers;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class ConfigurationHandler {
+    private static ConfigurationHandler instance;
     private final Properties properties;
     private final String filePath;
 
-    public ConfigurationHandler() {
-        this(ConfigurationHandler.class.getClassLoader().getResource("config.properties").getPath());
+    private ConfigurationHandler() {
+        this("config.properties");
     }
 
-    public ConfigurationHandler(String filePath) {
-        // The path to the .properties file
+    private ConfigurationHandler(String filePath) {
         this.filePath = filePath;
         this.properties = new Properties();
 
-        try (FileInputStream input = new FileInputStream(filePath)) {
-            properties.load(input);
+        try {
+            Path path = Paths.get(filePath);
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+
+            try (InputStream inputStream = new FileInputStream(filePath)) {
+                properties.load(inputStream);
+            }
         } catch (IOException ex) {
             System.err.printf("Error loading properties file: %s%n%n", ex.getMessage());
+        }
+    }
+
+    public static ConfigurationHandler getInstance() {
+        if (ConfigurationHandler.instance == null) {
+            ConfigurationHandler.instance = new ConfigurationHandler();
+        }
+
+        if (instance.getProperty("KEY_PATH") == null) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Please give the path to where the keys should be stored: ");
+                String keyPath = scanner.nextLine();
+
+                instance.setProperty("KEY_PATH", keyPath);
+                instance.saveProperties();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return ConfigurationHandler.instance;
+    }
+
+    private static void checkInitialisationFields() {
+        Scanner scanner = new Scanner(System.in);
+
+        if (instance.getProperty("KEY_PATH") == null) {
+            System.out.print("Please give the path to where the keys should be stored: ");
+            String keyPath = scanner.nextLine();
+
+            instance.setProperty("KEY_PATH", keyPath);
         }
     }
 
@@ -47,8 +88,8 @@ public class ConfigurationHandler {
      * Save the properties back to the .properties file.
      */
     public void saveProperties() {
-        try (FileOutputStream output = new FileOutputStream(filePath)) {
-            properties.store(output, "Updated properties");
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(filePath))) {
+            properties.store(writer, "Updated properties");
         } catch (IOException ex) {
             System.err.println("Error saving properties file: " + ex.getMessage());
         }
